9d6dba31f471da9aaa8b2c4a4491ab40c5c5743d
VK <vigneshkumar646@gmail.com>
2023-11-27 13:13:47 +0530

    PrimeVideoPage update [Day6]

--- src/pages/PrimeVideoPage/PrimeVideoPage.js
+++ src/pages/PrimeVideoPage/PrimeVideoPage.js
@@ -1,7 +1,27 @@
+import LatestVideoList from "./components/LatestVideoList";
+
 const PrimeVideoPage = () => {
   return (
-    <div>PrimeVideoPage</div>
-  )
-}
+    <div>
+      <h1>Prime Video App</h1>
 
-export default PrimeVideoPage
+      <h2>Latest Videos | Props Demo</h2>
+      <LatestVideoList />
+      <hr />
+
+      <h2>Trending Videos | States and Events Demo</h2>
+      <hr />
+
+      <h2>
+        Recommended Videos | States, Events, Conditional Rendering, Lists & Keys
+        Demo
+      </h2>
+      <hr />
+
+      <h2>Styling in React JS</h2>
+      <hr />
+    </div>
+  );
+};
+
+export default PrimeVideoPage;
