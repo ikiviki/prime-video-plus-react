59413d6dfbef1f2f13380f2b9c859961ac1dbc00
VK <vigneshkumar646@gmail.com>
2023-11-27 13:11:14 +0530

    simple props - parentToChild [Day6]

--- src/pages/PrimeVideoPage/components/LatestVideo.js
+++ src/pages/PrimeVideoPage/components/LatestVideo.js
@@ -1,14 +1,22 @@
-const LatestVideo = () => {
+const LatestVideo = (props) => {
+  // receiving data thru props
+  console.log(props);
+  /**
+   * props are objects
+   * props are read-only
+   * props are the way to receive data from parent comp
+   * props can have children property
+   */
   return (
     <div className="card">
-      <img src="..." className="card-img-top" alt="..." />
+      <img src={props.thumbnailUrl} className="card-img-top" alt={props.title} />
       <div className="card-body">
-        <h5 className="card-title">Video title</h5>
-        <p className="card-text">Video desc</p>
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.description}</p>
       </div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">Action</li>
-        <li className="list-group-item">published a day ago</li>
+        <li className="list-group-item">{props.category}</li>
+        <li className="list-group-item">{props.children}</li>
       </ul>
     </div>
   );
--- src/pages/PrimeVideoPage/components/LatestVideoList.js
+++ src/pages/PrimeVideoPage/components/LatestVideoList.js
@@ -1,18 +1,55 @@
 import LatestVideo from "./LatestVideo";
+const latestVideos = [
+  {
+    id: 45345,
+    title: "Formula 1 - Final",
+    description:
+      "Verstappen beats Leclerc to victory in Abu Dhabi to end record-breaking year on top",
+    thumbnailUrl: "https://placehold.co/350x250",
+    category: "Sports",
+    publishedOn: "2 hours ago",
+  },
+  {
+    id: 5467890,
+    title: "Greatest goals in the Premier League",
+    description:
+      "Garnacho's third-minute wonder goal muted the protesting Everton supporters at Goodison Park.",
+    thumbnailUrl: "https://placehold.co/350x250",
+    category: "Sports",
+    publishedOn: "5 hours ago",
+  },
+];
 
 const LatestVideoList = () => {
   return (
     <div className="row">
       <div className="col-md-3">
+        <LatestVideo
+          title={latestVideos[0].title}
+          description={latestVideos[0].description}
+          thumbnailUrl={latestVideos[0].thumbnailUrl}
+          category={latestVideos[0].category}
+        >
+          {latestVideos[0].publishedOn}
+          {/* Inner Text items are passed as 'children' => 'props.children' */}
+        </LatestVideo>
+      </div>
+      <div className="col-md-3">
+        <LatestVideo
+          title={latestVideos[1].title}
+          description={latestVideos[1].description}
+          thumbnailUrl={latestVideos[1].thumbnailUrl}
+          category={latestVideos[1].category}
+        >
+          {latestVideos[1].publishedOn}
+        </LatestVideo>
+      </div>
+      <div className="col-md-3">
         <LatestVideo />
       </div>
       <div className="col-md-3">
         <LatestVideo />
       </div>
-      <div className="col-md-3">col 3</div>
-      <div className="col-md-3">col 4</div>
-      <div className="col-md-3">col 5</div>
-      <div className="col-md-3">col 6</div>
     </div>
   );
 };
