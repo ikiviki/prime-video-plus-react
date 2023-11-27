171243d0e038f712b5a7f8310ee77fc7a6aad131
VK <vigneshkumar646@gmail.com>
2023-11-27 12:29:40 +0530

    simple data binding [Day6]

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,16 @@ import MenuList from "./MenuList";
 
 // FC with anonymous fn
 const Footer = function () {
+  const copyrightYear = 2023;
+  const developerName = "VK";
   // must return JSX
   return (
     <footer className="text-center">
       <hr />
       <MenuList />
-      <p>&copy; Copyright 2023 | VK</p>
+      <p>
+        &copy; Copyright {copyrightYear} | {developerName}
+      </p>
     </footer>
   );
 };
