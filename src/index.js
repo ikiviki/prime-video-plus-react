d7f7a14336915cb552e8b1933a682da0706886ed
VK <vigneshkumar646@gmail.com>
2023-11-28 10:39:31 +0530

    grouping css together [Day7-review]

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import React from "react"; // Import official react library.
+import ReactDOM from "react-dom/client"; // Importing react-dom to work with the DOM.
 import 'bootstrap/dist/css/bootstrap.css'; // Tightly Integrating Bootstrap (RECOMMENDED)
 /**
  * Put any other imports below so that CSS from your
  * components takes precedence over default styles.
  */
-import React from "react"; // Import official react library.
-import ReactDOM from "react-dom/client"; // Importing react-dom to work with the DOM.
 import "./index.css"; // Importing css.
 import App from "./App"; // Importing App Component.
 // Below: pi, add is named export and its name cannot be changed, also this Object should be DESTRUCTURED hence inside { XX }.
