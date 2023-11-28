7a658ab860e7b2d208e23d400eb24799b7e5e16a
VK <vigneshkumar646@gmail.com>
2023-11-28 13:23:25 +0530

    States example [Day7]

--- src/pages/PrimeVideoPage/components/TrendingVideoList.js
+++ src/pages/PrimeVideoPage/components/TrendingVideoList.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 
 const TrendingVideoList = () => {
+  // State is a component wide updateable data.
+  // Whenever the state is updated -> the UI will update.
+  const [resolution, setResolution] = useState("HD");
+  const handleResolution = () => {
+    // Updating the state using the setter.
+    setResolution("4k");
+  };
+
   return (
     <div className="row">
+      <h4>
+        Enjoy Watching in {resolution} | &nbsp;
+        <button className="btn btn-primary btn-sm" onClick={handleResolution}>
+          Change Resolution
+        </button>
+      </h4>
       <div className="col-md-3">col1</div>
       <div className="col-md-3">col2</div>
     </div>
