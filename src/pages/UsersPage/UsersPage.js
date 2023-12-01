04a80079e30db65dd2b4c600341a40e6b363eb30
VK <vigneshkumar646@gmail.com>
2023-12-01 12:42:40 +0530

    All UsersPage display [Day10]

--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -1,7 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 
 const UsersPage = () => {
+  const [users, setUsers] = useState([]); // Positive state.
+  const [isLoading, setIsLoading] = useState(true); // Loading state.
+  const [isError, setIsError] = useState([]); // Negative state.
+
+  // Will be called only after INITIAL RENDERING.
+  useEffect(() => {
+    // Ideal place for us to connect to REST API.
+    console.log("Inside userEffect");
+    /*
+      What's the REST API URL? https://jsonplaceholder.typicode.com/users
+      What's the HTTP Method? GET
+      What's the REST API Client Tool? axios
+      Should we send any form data? NO
+    */
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        console.log(response.data); // Successful response.
+        setUsers(response.data);
+      } catch (err) {
+        console.log(err); // Error response.
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []); // You can have dependency list in this array.
+
   return (
     <>
       <Helmet>
@@ -27,21 +61,25 @@ const UsersPage = () => {
 
       <div className="row">
         <h2>Listing Users</h2>
-        <div className="col-md-3 mt-2">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Name: John</h5>
-              <p className="card-text">E-Mail: j@k.com</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <Link to="/users/1" className="btn-link">
-                    View Details
-                  </Link>
+        {users.map((user) => {
+          return (
+            <div className="col-md-3 mt-2" key={user.id}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{user.name}</h5>
+                  <p className="card-text">E-Mail: {user.email}</p>
+                  <div className="card-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <Link to={`/users/${user.id}`} className="btn-link">
+                        View Details
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
