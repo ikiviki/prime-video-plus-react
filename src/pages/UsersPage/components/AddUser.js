c4513cbc7d684a53205fdf579507056774349280
VK <vigneshkumar646@gmail.com>
2023-12-08 14:40:59 +0530

    react-hook-form Validation [TODO-10]
    Pending:
    disable the submit button if form is invalid

--- src/pages/UsersPage/components/AddUser.js
+++ src/pages/UsersPage/components/AddUser.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
@@ -9,8 +10,23 @@ const AddUser = () => {
     phone: "",
     email: "",
   });
+
   const [submitButton, setSubmitButton] = useState("Create User");
   const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      phone: "",
+      email: "",
+    },
+  });
+
+  // console.log(errors); // Form Validation error messages.
 
   // const [isSaved, setIsSaved] = useState(false);
   // const [isError, setIsError] = useState(false);
@@ -22,8 +38,9 @@ const AddUser = () => {
     });
   };
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
+  const handleFormSubmit = (data) => {
+    // const handleSubmit = (event) => {
+    // event.preventDefault();
     // console.log(addUserForm); // submittable form data to the REST API
 
     // Let's send the above data to the REST API
@@ -31,7 +48,9 @@ const AddUser = () => {
     // 2. What's the HTTP Method? POST
     // 3. What's the REST API Client Tool? axios (npm i axios) or fetch
     // 4. What's the form data? addUserForm
+    // debugger;
     toast.promise(
+      // axios.post("https://jsonplaceholder.typicode.com/users", data),
       axios.post("https://jsonplaceholder.typicode.com/users", addUserForm),
       // .then((res) => {
       // console.log(res);
@@ -47,14 +66,22 @@ const AddUser = () => {
       {
         loading: () => {
           setSubmitButton("Creating User...");
+          document.getElementById("submitButton").disabled = true;
           return "Submitting Form";
         },
         success: () => {
           setSubmitButton("Create User");
+          setAddUserForm({
+            name: "",
+            phone: "",
+            email: "",
+          });
+          document.getElementById("submitButton").disabled = false;
           return "POST Success !!";
         },
         error: () => {
           setSubmitButton("Create User");
+          document.getElementById("submitButton").disabled = false;
           return "Unable to create user. Try again later.";
         },
         // error: (err) => err.message,
@@ -67,7 +94,15 @@ const AddUser = () => {
   return (
     <div className="row">
       <h1>Add User</h1>
-      <form className="col-md-4 offset-md-4" onSubmit={handleFormSubmit}>
+      {/* Normal Form Submit */}
+      {/* <form className="col-md-4 offset-md-4" onSubmit={handleFormSubmit}> */}
+      {/* React Hook Form Submit */}
+      <form
+        className="col-md-4 offset-md-4"
+        onSubmit={handleSubmit((data) => {
+          handleFormSubmit(data);
+        })}
+      >
         <button
           type="button"
           className="btn btn-outline-secondary mb-3"
@@ -80,6 +115,7 @@ const AddUser = () => {
             Name
           </label>
           <input
+            {...register("name", { required: "* Required" })}
             type="text"
             className="form-control"
             id="nameInput"
@@ -88,12 +124,17 @@ const AddUser = () => {
             value={addUserForm.name}
             onChange={handleChange}
           />
+          <p className="text-danger">{errors.name?.message}</p>
         </div>
         <div className="mb-3">
           <label htmlFor="phoneInput" className="form-label">
             Phone
           </label>
           <input
+            {...register("phone", {
+              required: "* Required",
+              maxLength: { value: 10, message: "Max length is 10" },
+            })}
             type="text"
             className="form-control"
             id="phoneInput"
@@ -102,12 +143,20 @@ const AddUser = () => {
             value={addUserForm.phone}
             onChange={handleChange}
           />
+          <p className="text-danger">{errors.phone?.message}</p>
         </div>
         <div className="mb-3">
           <label htmlFor="emailInput" className="form-label">
             Email address
           </label>
           <input
+            {...register("email", {
+              required: "* Required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Please enter a valid email",
+              },
+            })}
             type="email"
             className="form-control"
             id="emailInput"
@@ -116,6 +165,7 @@ const AddUser = () => {
             value={addUserForm.email}
             onChange={handleChange}
           />
+          <p className="text-danger">{errors.email?.message}</p>
         </div>
 
         {/* {isSaved && <div className="alert alert-success">POST Success !!</div>} */}
@@ -126,7 +176,7 @@ const AddUser = () => {
         )} */}
         <Toaster />
 
-        <button type="submit" className="btn btn-primary">
+        <button id="submitButton" type="submit" className="btn btn-primary">
           {submitButton}
         </button>
       </form>
